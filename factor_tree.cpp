788e08b37f4438024f775ddaf5fa3d465bd2b605
meichendavid <meichendavid@users.noreply.github.com>
2017-12-24 10:02:06 -0600

    First commit

---
+++ factor_tree.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+void print_factor_tree()
+{
+	// Recursive, by finding the common greatest divisor of each and print each tree recurisvely as well 
+
+}
+
+int main()
+{
+	
+	return 0;
+}
