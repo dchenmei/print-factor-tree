649e42c7e076644620fc7ae1ec9833bf2b5ed868
meichendavid <meichendavid@users.noreply.github.com>
2017-12-30 11:56:44 -0600

    Fixed infinite loop bug

--- factor_tree.cpp
+++ factor_tree.cpp
@@ -41,51 +41,34 @@ void Factor_Tree::construct_factor_tree(int r)
 	vector<int> not_factored;
 	not_factored.push_back(r); /* Push root to start */
 	
-	int i = 5;	
-	int curr;
-	while (i--)
+	while (!not_factored.empty())
 	{   
-		cout << i << endl;
-		for (int x : not_factored)
-		{
-			cout << x << " ";
-		}
-		cout << endl;
 		/* Instead of using the for loop, just keep checking the head to prevent
 *                out of bound shit storm */
 
 		/* Process and pop value */
-		curr = not_factored[0];
-		cout << "curr: " << curr << endl;
-
-
+		int curr = not_factored[0];
 		factor_tree.push_back(curr); /* Add to factor tree */
 
 		/* If not prime value, we need to factor further */
-		if (greatest_factor(curr) != 0)
+		if (greatest_factor(curr))
 		{
-		    cout << "X w/:" << greatest_factor(curr) <<  endl;
-			curr = greatest_factor(curr);
-			not_factored.push_back(curr);
-			not_factored.push_back(r / curr);
-		
+            int curr_factor = greatest_factor(curr);
+			not_factored.push_back(curr_factor);
+			not_factored.push_back(curr / curr_factor);
 		}
 	
 		not_factored.erase(not_factored.begin()); /* runs O(n), don't worry about it */
-
 	}
-
 }
 
 int main()
 {
 	Factor_Tree *tree = new Factor_Tree(48, false);
-	/*
 	vector<int> tv = tree->get_factor_tree();
 
 	for (int x : tv)
 		cout << x << " ";
-	*/
 	
 	return 0;
 }
